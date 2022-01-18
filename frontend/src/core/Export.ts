6488e0e7e24c07e6e2006cbf77183e0fc24b02a5
benny <benny@LT-BENNY>
2022-01-18 14:28:20 +0200

    Are you sure main dialog

--- frontend/src/core/Export.ts
+++ frontend/src/core/Export.ts
@@ -2,4 +2,5 @@ export * from './AppConstants';
 export * from './ApiService';
 export * from '../boot/i18n';
 export * from '../utils/notification-utils';
+export * from '../utils/dialog-utils';
 export * from '../services/functions.service';
