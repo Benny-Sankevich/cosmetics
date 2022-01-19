1bdda4e307116e63d69714b68db42d7abd095e04
benny <benny@LT-BENNY>
2022-01-19 12:14:10 +0200

    Update mongoose version and the connection to the DB

--- Backend/data-access-layer/dal.js
+++ Backend/data-access-layer/dal.js
@@ -1,19 +1,5 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-function connectAsync() {
-    return new Promise((resolve, reject) => {
-        const options = { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true };
-        mongoose.connect(config.mongodb.connectionString, options, (err, db) => {
-            if (err) return reject(err);
-            resolve(db);
-        });
-    });
-}
-(async () => {
-    try {
-        await connectAsync();
-    }
-    catch (err) {
-        console.log(err);
-    }
-})();
+mongoose
+    .connect(global.config.mongodb.connectionString)
+    .catch((err) => console.log(err));
