83d4e5c5b8eade609bca8048150a560f9c6f8ee5
benny <benny@LT-BENNY>
2022-01-24 15:11:46 +0200

    fix helper summeries

--- Backend/business-logic-layer/summaries-logic.js
+++ Backend/business-logic-layer/summaries-logic.js
@@ -1,6 +1,7 @@
 require("../data-access-layer/dal");
 const SummariesModel = require("../models/summaries-model");
-const summariesHelper = require("../helpers/summaries-helper");
+const treatmentLogic = require("./treatment-logic");
+const helpers = require("../helpers/helpers");
 
 function getAllSummariesByYear(year) {
     return SummariesModel.find({ year }).exec()
@@ -25,18 +26,56 @@ async function updateSummariesData(dataType, treatmentId, year, month, monthlySu
     let condition = treatmentId ? { treatmentId } : { dataType };
     let existingSummary = await getSummaryByYear(year, condition);
     if (!existingSummary) {
-        const newSummaries = await summariesHelper.getDefaultSummariesData(dataType, year, treatmentId);
+        const newSummaries = await getDefaultSummariesData(dataType, year, treatmentId);
         existingSummary = await addSummaries(newSummaries);
     }
     existingSummary.data[month - 1] = monthlySummary;
     return SummariesModel.findByIdAndUpdate(existingSummary._id, existingSummary, { returnOriginal: false }).exec();
 }
 
+function getDefaultSummariesData(dataType, year, treatmentId) {
+    if (treatmentId) return getTreatmentSummaryData(year, treatmentId)
+    return getSummaryData(dataType, year);
+}
+async function getTreatmentSummaryData(year, treatmentId) {
+    let treatment = await getSummaryByTreatmentId(treatmentId);
+    if (treatment) {
+        return getDefaultSummaries('treatment', treatment.type, treatment.label, treatmentId, year, treatment.borderWidth, undefined, treatment.backgroundColor, treatment.stack, undefined);
+    }
+    else {
+        treatment = await treatmentLogic.getTreatmentByIdAsync(treatmentId);
+        return getDefaultSummaries('treatment', 'bar', treatment.name, treatmentId, year, 1, null, null, 'Stack 3', null);
+    }
+}
+async function getSummaryData(dataType, year) {
+    const summary = await getSummaryByDataType(dataType);
+    if (summary) {
+        return getDefaultSummaries(dataType, summary.type, summary.label, null, year, null, summary.borderColor, summary.backgroundColor, null, false);
+    }
+    const randomColor = helpers.getRandomColor();
+    return getDefaultSummaries(dataType, 'line', dataType, null, year, null, randomColor, randomColor, null, false);
+
+}
+
+function getDefaultSummaries(dataType, type, label, treatmentId, year, borderWidth, borColor, bgColor, stack, fill) {
+    return new SummariesModel({
+        dataType,
+        type,
+        label,
+        treatmentId,
+        year,
+        data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        borderWidth: borderWidth ? borderWidth : undefined,
+        borderColor: borColor ? borColor : undefined,
+        backgroundColor: bgColor ? bgColor : helpers.getRandomColor(),
+        stack: stack ? stack : undefined,
+        fill: fill === false ? fill : undefined,
+    });
+}
+
 
 module.exports = {
     getAllSummariesByYear,
     updateSummariesData,
-    getSummaryByDataType,
-    getSummaryByTreatmentId,
     getSummaryByDataTypeOrTreatmentIdAsync
 }
